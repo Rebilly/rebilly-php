6046381bd31ef874a0175db6b658aee6b1a6659b
dara <dara@ilabsinc.com>
2015-02-11 16:42:27 -0500

    payment card resource

--- lib/v2_1/PaymentCard.php
+++ lib/v2_1/PaymentCard.php
@@ -12,13 +12,41 @@ use Exception;
  * Usage:
  * ~~~
  * // Get all card belong to a customer
- * $transaction = new Rebilly\v2_1\PaymentCard("customerId");
- * $transaction->setEnvironment(RebillyRequest::ENV_SANDBOX);
- * $transaction->setApiKey('apiKey');
+ * $card = new Rebilly\v2_1\PaymentCard("customerId");
+ * $card->setEnvironment(RebillyRequest::ENV_SANDBOX);
+ * $card->setApiKey('apiKey');
  *
- * $response = $transaction->listAll();
+ * $response = $card->listAll();
  * if ($response.statusCode === 200) {
+ *     // Success
+ * }
+ * ~~~
  *
+ * ~~~
+ * // Create new payment card
+ * $card = new Rebilly\v2_1\PaymentCard("customerId");
+ * $card->setEnvironment(RebillyRequest::ENV_SANDBOX);
+ * $card->setApiKey('apiKey');
+ * $card->pan = "4111111111111111";
+ * $card->expMonth = "02";
+ * $card->expYear = "2018";
+ * $card->contact = "contact123ABC";
+ *
+ * $response = $card->create();
+ * if ($response.statusCode === 201) {
+ *     // Success
+ * }
+ * ~~~
+ *
+ * ~~~
+ * // Deactivate the payment card
+ * $card = new Rebilly\v2_1\PaymentCard("customerId", "paymentCardId");
+ * $card->setEnvironment(RebillyRequest::ENV_SANDBOX);
+ * $card->setApiKey('apiKey');
+ *
+ * $response = $card->deactivate();
+ * if ($response.statusCode === 200) {
+ *     // Success
  * }
  * ~~~
  */
@@ -26,6 +54,19 @@ class PaymentCard extends RebillyRequest
 {
     const CARD_END_POINT = '/payment-cards/';
     const CUSTOMER_END_POINT = 'customers/';
+
+    /** @var int $pan Primary Account Number */
+    public $pan;
+    /** @var int $expMonth */
+    public $expMonth;
+    /** @var int $expYear */
+    public $expYear;
+    /** @var string $contact */
+    public $contact;
+    /** @var int $cvv */
+    public $cvv;
+    /** @var string $token */
+    public $token;
 
     /** @var string $customerId */
     private $customerId;
@@ -68,6 +109,58 @@ class PaymentCard extends RebillyRequest
 
         return $this->sendGetRequest();
     }
+
+    /**
+     * Create new payment card with auto generated ID
+     * @return RebillyResponse
+     */
+    public function create()
+    {
+        $this->setApiController(self::CUSTOMER_END_POINT . $this->customerId . self::CARD_END_POINT);
+
+        $data = $this->buildRequest($this);
+
+        return $this->sendPostRequest($data);
+    }
+
+    /**
+     * Create new payment card with ID
+     * @return RebillyResponse
+     */
+    public function update()
+    {
+        if (empty($this->id)) {
+            throw new Exception('Card ID cannot be empty.');
+        }
+        $this->setApiController(self::CUSTOMER_END_POINT . $this->customerId . self::CARD_END_POINT . $this->id);
+
+        $data = $this->buildRequest($this);
+
+        return $this->sendPutRequest($data);
+    }
+
+    /**
+     * Do an authorization on the payment card
+     * @return RebillyResponse
+     */
+    public function authorize()
+    {
+        $this->setApiController(self::CUSTOMER_END_POINT . $this->customerId . self::CARD_END_POINT . 'authorization/');
+
+        return $this->sendPostRequest(null);
+    }
+
+    /**
+     * Deactivate a payment card
+     * @return RebillyResponse
+     */
+    public function deactivate()
+    {
+        $this->setApiController(self::CUSTOMER_END_POINT . $this->customerId . self::CARD_END_POINT . 'deactivation/');
+
+        return $this->sendPostRequest(null);
+    }
+
     /**
      * Return all the property of this class
      * @param $class
