377a4dfa600dedde46e55c0349068a5302715662
dara <dara@ilabsinc.com>
2015-03-09 16:14:47 -0400

    Check object and add comments

--- lib/RebillyRequest.php
+++ lib/RebillyRequest.php
@@ -272,17 +272,27 @@ abstract class RebillyRequest
     {
         $data = array();
         $publicProperties = array();
-        $objectArray = is_object($object) ? $this->getPublicProperties($object) : $object;
-        $properties = (new ReflectionObject($object))->getProperties(ReflectionProperty::IS_PUBLIC);
-        foreach ($properties as $property) {
-            $publicProperties[$property->name] = $property->name;
+
+        if (is_object($object)) {
+            // Get all properties of the class and value
+            $objectArray = $this->getPublicProperties($object);
+            // Get only public properties of the class -- name only
+            $properties = (new ReflectionObject($object))->getProperties(ReflectionProperty::IS_PUBLIC);
+            foreach ($properties as $property) {
+                $publicProperties[$property->name] = $property->name;
+            }
+        } else {
+            $objectArray = $object;
         }
+
         foreach ($objectArray as $key => $value) {
             $value = (is_array($value) || is_object($value)) ? $this->buildRequest($value, false) : $value;
+            // assign value if it's not empty and is public property
             if (!empty($value) && isset($publicProperties[$key])) {
                 $data[$key] = $value;
             }
         }
+
         if (!$asJson) {
             return $data;
         }
