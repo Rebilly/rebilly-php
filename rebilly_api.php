bff5f081de14c1ebd534eb9032988af1473c6929
slavcodev <slavcopost@gmail.com>
2015-06-02 19:59:13 +0300

    Rvert to use autoload file

--- rebilly_api.php
+++ rebilly_api.php
@@ -1,32 +1,42 @@
 <?php
-/**
- * Register `Rebilly` root namespace and link to `lib` directory.
- * All classes without namespace will load from base directory.
- */
+// Common classes
+require_once(__DIR__ . '/lib/HttpClient/HttpClient.php');
+require_once(__DIR__ . '/lib/HttpClient/CurlAdapter/CurlAdapter.php');
+require_once(__DIR__ . '/lib/HttpClient/CurlAdapter/CurlError.php');
+require_once(__DIR__ . '/lib/util/RebillyHttpStatusCode.php');
 
-require_once __DIR__ . '/lib/RebillyClassLoader.php';
+// Require all Rebilly base class - v2
+require_once(__DIR__ . '/lib/RebillyAddressInfo.php');
+require_once(__DIR__ . '/lib/RebillyBlacklist.php');
+require_once(__DIR__ . '/lib/RebillyCharge.php');
+require_once(__DIR__ . '/lib/RebillyCustomer.php');
+require_once(__DIR__ . '/lib/RebillyCustomField.php');
+require_once(__DIR__ . '/lib/RebillyDispute.php');
+require_once(__DIR__ . '/lib/RebillyLayout.php');
+require_once(__DIR__ . '/lib/RebillyPaymentCard.php');
+require_once(__DIR__ . '/lib/RebillyPlan.php');
+require_once(__DIR__ . '/lib/RebillyRequest.php');
+require_once(__DIR__ . '/lib/RebillyResponse.php');
+require_once(__DIR__ . '/lib/RebillySignature.php');
+require_once(__DIR__ . '/lib/RebillySubscription.php');
+require_once(__DIR__ . '/lib/RebillyThreeDSecure.php');
+require_once(__DIR__ . '/lib/RebillyToken.php');
+require_once(__DIR__ . '/lib/RebillyTransaction.php');
 
-$loader = new RebillyClassLoader(
-    'Rebilly',
-    __DIR__ . '/lib',
-    [
-        'RebillyHttpStatusCode' => __DIR__ . '/lib/util/RebillyHttpStatusCode.php',
-        'RebillyRequest' => __DIR__ . '/lib/RebillyRequest.php',
-        'RebillyResponse' => __DIR__ . '/lib/RebillyResponse.php',
-        'RebillyAddressInfo' => __DIR__ . '/lib/RebillyAddressInfo.php',
-        'RebillyBlacklist' => __DIR__ . '/lib/RebillyBlacklist.php',
-        'RebillyCharge' => __DIR__ . '/lib/RebillyCharge.php',
-        'RebillyCustomer' => __DIR__ . '/lib/RebillyCustomer.php',
-        'RebillyDispute' => __DIR__ . '/lib/RebillyDispute.php',
-        'RebillyPaymentCard' => __DIR__ . '/lib/RebillyPaymentCard.php',
-        'RebillySignature' => __DIR__ . '/lib/RebillySignature.php',
-        'RebillySubscription' => __DIR__ . '/lib/RebillySubscription.php',
-        'RebillyTransaction' => __DIR__ . '/lib/RebillyTransaction.php',
-        'RebillyToken' => __DIR__ . '/lib/RebillyToken.php',
-        'RebillyThreeDSecure' => __DIR__ . '/lib/RebillyThreeDSecure.php',
-        'RebillyPlan' => __DIR__ . '/lib/RebillyPlan.php',
-        'RebillyLayout' => __DIR__ . '/lib/RebillyLayout.php',
-        'RebillyCustomField' => __DIR__ . '/lib/RebillyCustomField.php',
-    ]
-);
-$loader->register();
+// v2.1
+require_once(__DIR__ . '/lib/v2_1/Blacklist.php');
+require_once(__DIR__ . '/lib/v2_1/Contact.php');
+require_once(__DIR__ . '/lib/v2_1/Customer.php');
+require_once(__DIR__ . '/lib/v2_1/Dispute.php');
+require_once(__DIR__ . '/lib/v2_1/Invoice.php');
+require_once(__DIR__ . '/lib/v2_1/InvoiceItem.php');
+require_once(__DIR__ . '/lib/v2_1/Layout.php');
+require_once(__DIR__ . '/lib/v2_1/LeadSource.php');
+require_once(__DIR__ . '/lib/v2_1/Payment.php');
+require_once(__DIR__ . '/lib/v2_1/PaymentCard.php');
+require_once(__DIR__ . '/lib/v2_1/PaymentCardInstrument.php');
+require_once(__DIR__ . '/lib/v2_1/Plan.php');
+require_once(__DIR__ . '/lib/v2_1/Signature.php');
+require_once(__DIR__ . '/lib/v2_1/Subscription.php');
+require_once(__DIR__ . '/lib/v2_1/Token.php');
+require_once(__DIR__ . '/lib/v2_1/Transaction.php');
