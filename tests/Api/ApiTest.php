5cd731fbce437aff9ea40020da268121cea3531d
slavcodev <slavcopost@gmail.com>
2015-08-26 01:50:30 +0300

    Entites tests

---
+++ tests/Api/ApiTest.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * This file is part of the PHP Rebilly API package.
+ *
+ * (c) 2015 Rebilly SRL
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Rebilly\Tests\Api;
+
+use InvalidArgumentException;
+use Rebilly\Entities;
+use Rebilly\Rest;
+
+/**
+ * Class ApiTest.
+ *
+ * @group wip
+ *
+ * @author Veaceslav Medvedev <veaceslav.medvedev@rebilly.com>
+ */
+class ApiTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider provideEntityClasses
+     *
+     * @param string $class
+     * @param string $id
+     */
+    public function buildJson($class, $id = 'id')
+    {
+        $getters = [];
+        $setters = [];
+        $values = [];
+
+        if ($id !== null) {
+            $values[$id] = $this->getFakeValue($id, $class);
+            $values['id'] = $values[$id];
+        }
+
+        /** @var Rest\Resource $resource */
+        $resource = new $class($values);
+
+        $methods = get_class_methods($resource);
+
+        foreach ($methods as $method) {
+            $prefix = substr($method, 0, 3);
+
+            if ($prefix === 'get') {
+                $attribute = lcfirst(substr($method, 3));
+                $getters[$attribute] = $method;
+            } elseif ($prefix === 'set') {
+                $attribute = lcfirst(substr($method, 3));
+                $setters[$attribute] = $method;
+            }
+        }
+
+        foreach ($setters as $attribute => $method) {
+            $values[$attribute] = $this->getFakeValue($attribute, $class);
+            $resource->$method($values[$attribute]);
+        }
+
+        foreach ($getters as $attribute => $method) {
+            if (isset($values[$attribute])) {
+                $this->assertEquals($values[$attribute], $resource->$method());
+            } else {
+                $this->assertNull($resource->$method());
+            }
+        }
+
+        $json = $resource->jsonSerialize();
+
+        $this->assertTrue(is_array($json));
+        $this->assertNotEmpty($json);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideEntityClasses()
+    {
+        return [
+            [Entities\AuthenticationOptions::class, null],
+            [Entities\AuthenticationToken::class, 'token'],
+            [Entities\Blacklist::class],
+            [Entities\Contact::class],
+            [Entities\Customer::class],
+            [Entities\CustomerCredential::class],
+            [Entities\Invoice::class],
+            [Entities\InvoiceItem::class],
+            [Entities\Layout::class],
+            [Entities\LeadSource::class],
+            [Entities\Payment::class],
+            [Entities\PaymentMethods\PaymentCardMethod::class, null],
+            [Entities\PaymentMethods\PaypalMethod::class, null],
+            [Entities\PaymentCard::class],
+            [Entities\PaymentCardAuthorization::class, null],
+            [Entities\PaymentCardToken::class, null],
+            [Entities\Plan::class],
+            [Entities\ResetPasswordToken::class, 'token'],
+            [Entities\ScheduledPayment::class],
+            [Entities\Subscription::class],
+            [Entities\SubscriptionSwitch::class, null],
+            [Entities\SubscriptionCancel::class, null],
+            [Entities\Transaction::class],
+            [Entities\Website::class],
+        ];
+    }
+
+    /**
+     * @param string $attribute
+     * @param string $class
+     *
+     * @return mixed
+     */
+    private function getFakeValue($attribute, $class)
+    {
+        $faker = $this->getFaker();
+
+        $faker->phoneNumber;
+
+        switch ($attribute) {
+            case 'id':
+            case 'password':
+            case 'customerId':
+            case 'websiteId':
+            case 'initialInvoiceId':
+            case 'billingContactId':
+            case 'deliveryContactId':
+            case 'planId':
+            case 'processorAccountId':
+            case 'paymentCardId':
+            case 'gatewayAccountId':
+            case 'defaultCardId':
+                return $faker->uuid;
+            case 'dueTime':
+            case 'expiredTime':
+            case 'expireTime': // TODO inconsistent name
+            case 'periodStartTime':
+            case 'renewalTime':
+            case 'periodEndTime':
+                return $faker->date('Y-m-d H:i:s');
+            case 'unitPrice':
+            case 'amount':
+            case 'recurringAmount':
+            case 'trialAmount':
+            case 'setupAmount':
+                return $faker->randomFloat(2);
+            case 'checkoutPageUri':
+                return $faker->word;
+            case 'organization':
+                return $faker->company;
+            case 'servicePhone':
+                return $faker->phoneNumber;
+            case 'serviceEmail':
+                return $faker->email;
+            case 'region':
+                return $faker->city;
+            case 'ipAddress':
+                return $faker->ipv4;
+            case 'token':
+            case 'fingerprint':
+            case 'paypalKey':
+                return $faker->md5;
+            case 'name':
+            case 'medium':
+            case 'source':
+            case 'campaign':
+            case 'term':
+            case 'content':
+            case 'affiliate':
+            case 'subAffiliate':
+            case 'salesAgent':
+            case 'clickId':
+            case 'path':
+                return $faker->words;
+            case 'description':
+                return $faker->sentences;
+            case 'pan':
+                return $faker->creditCardNumber;
+            case 'cvv':
+                return $faker->numberBetween(100, 999);
+            case 'expYear':
+                return $faker->year;
+            case 'expMonth':
+                return $faker->month;
+            case 'webHookUrl':
+                return $faker->url;
+            case 'webHookUsername':
+                return $faker->userName;
+            case 'webHookPassword':
+                return $faker->md5;
+            case 'isActive':
+                return $faker->boolean();
+            case 'credentialTtl':
+            case 'authTokenTtl':
+            case 'resetTokenTtl':
+            case 'quantity':
+            case 'recurringPeriodLength':
+            case 'trialPeriodLength':
+            case 'contractTermLength':
+            case 'recurringPeriodLimit':
+            case 'minQuantity':
+            case 'maxQuantity':
+                return $faker->numberBetween(1, 10);
+            case 'address2':
+                return $faker->address;
+            case 'postalCode':
+                return $faker->postcode;
+            case 'email':
+            case 'firstName':
+            case 'lastName':
+            case 'username':
+            case 'url':
+            case 'address':
+            case 'city':
+            case 'country':
+            case 'phoneNumber':
+                return $faker->$attribute;
+            case 'type':
+                switch ($class) {
+                    case Entities\Blacklist::class:
+                        return $faker->randomElement(Entities\Blacklist::types());
+                    case Entities\InvoiceItem::class:
+                        return $faker->randomElement(Entities\InvoiceItem::types());
+                    default:
+                        throw new InvalidArgumentException(
+                            sprintf('Cannot generate fake value for "%s :: %s"', $class, $attribute)
+                        );
+                }
+            case 'value':
+                switch ($class) {
+                    case Entities\Blacklist::class:
+                        return $faker->word;
+                    default:
+                        throw new InvalidArgumentException(
+                            sprintf('Cannot generate fake value for "%s :: %s"', $class, $attribute)
+                        );
+                }
+            case 'policy':
+                switch ($class) {
+                    case Entities\SubscriptionCancel::class:
+                    case Entities\SubscriptionSwitch::class:
+                        return $faker->randomElement(Entities\SubscriptionCancel::policies());
+                    default:
+                        throw new InvalidArgumentException(
+                            sprintf('Cannot generate fake value for "%s :: %s"', $class, $attribute)
+                        );
+                }
+            case 'recurringPeriodUnit':
+            case 'trialPeriodUnit':
+            case 'contractTermUnit':
+                return 'day'; // TODO
+            case 'passwordPattern':
+                return '/\w\d{6,}/';
+            case 'currency':
+                return 'USD';
+            case 'payment':
+                return []; // TODO
+            case 'method':
+                return new Entities\PaymentMethods\PaymentCardMethod(); // TODO
+            default:
+                throw new InvalidArgumentException(
+                    sprintf('Cannot generate fake value for "%s :: %s"', $class, $attribute)
+                );
+        }
+    }
+}
