d7fa56bb53ea7f5dad2b0465a5e0718716b0f621
Xaos <xaossintez@gmail.com>
2015-09-11 18:45:59 +0600

    Add Token create and expire methods

--- src/Services/PaymentCardTokenService.php
+++ src/Services/PaymentCardTokenService.php
@@ -11,8 +11,10 @@
 namespace Rebilly\Services;
 
 use ArrayObject;
+use JsonSerializable;
 use Rebilly\Entities\PaymentCardToken;
 use Rebilly\Http\Exception\NotFoundException;
+use Rebilly\Http\Exception\UnprocessableEntityException;
 use Rebilly\Rest\Collection;
 use Rebilly\Rest\Service;
 
@@ -24,6 +26,23 @@ use Rebilly\Rest\Service;
  */
 final class PaymentCardTokenService extends Service
 {
+    /**
+     * @param array|JsonSerializable|PaymentCardToken $data
+     * @param string $tokenId
+     *
+     * @throws UnprocessableEntityException The input data does not valid
+     *
+     * @return PaymentCardToken
+     */
+    public function create($data, $tokenId = null)
+    {
+        if (isset($tokenId)) {
+            return $this->client()->put($data, 'tokens/{tokenId}', ['tokenId' => $tokenId]);
+        } else {
+            return $this->client()->post($data, 'tokens');
+        }
+    }
+
     /**
      * @param array|ArrayObject $params
      *
@@ -45,5 +64,18 @@ final class PaymentCardTokenService extends Service
     public function load($tokenId, $params = [])
     {
         return $this->client()->get('tokens/{tokenId}', ['tokenId' => $tokenId] + (array) $params);
+    }
+
+    /**
+     * @param string $tokenId
+     * @param array|ArrayObject $params
+     *
+     * @throws NotFoundException The resource data does exist
+     *
+     * @return PaymentCardToken
+     */
+    public function expire($tokenId, $params = [])
+    {
+        return $this->client()->post(['tokenId' => $tokenId] + (array) $params, 'tokens/'.$tokenId.'/expiration');
     }
 }
