2759cb95dd78d1c45214f6ab253282e4bf964a39
Bogdan L <thaddeusbox@gmail.com>
2016-10-19 16:46:33 +0600

    Fix tests

--- tests/Api/ApiTest.php
+++ tests/Api/ApiTest.php
@@ -1199,6 +1199,8 @@ class ApiTest extends TestCase
                 return [
                     'method' => Entities\PaymentMethod::METHOD_PAYMENT_CARD,
                 ];
+            case 'retryInstruction':
+                return new Entities\PaymentRetryInstruction();
             case 'reasonCode':
                 return '1000';
             case 'status':
