3e96b3c60429cdc54de33a3315abfc6f8cfa2df6
Gleb <wmdandroid@gmail.com>
2017-03-01 22:44:37 +0300

    Add few another get methods into Transaction (#214)
    * Add few another get methods into Transaction
    
    * Сlean up code

--- src/Entities/Transaction.php
+++ src/Entities/Transaction.php
@@ -193,4 +193,84 @@ final class Transaction extends Entity
     {
         return $this->getAttribute('invoiceIds');
     }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->getAttribute('status');
+    }
+
+    /**
+     * @return array
+     */
+    public function getChildTransactions()
+    {
+        return $this->getAttribute('childTransactions');
+    }
+
+    /**
+     * @return string
+     */
+    public function getGatewayAccountId()
+    {
+        return $this->getAttribute('gatewayAccountId');
+    }
+
+    /**
+     * @return string
+     */
+    public function getGatewayTransactionId()
+    {
+        return $this->getAttribute('gatewayTransactionId');
+    }
+
+    /**
+     * @return string
+     */
+    public function getGatewayName()
+    {
+        return $this->getAttribute('gatewayName');
+    }
+
+    /**
+     * @return string
+     */
+    public function getAcquirerName()
+    {
+        return $this->getAttribute('acquirerName');
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->getAttribute('method');
+    }
+
+    /**
+     * @return string
+     */
+    public function getBin()
+    {
+        return $this->getAttribute('bin');
+    }
+
+    /**
+     * @return bool
+     */
+    public function has3ds()
+    {
+        return $this->getAttribute('has3ds');
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasDcc()
+    {
+        return $this->getAttribute('hasDcc');
+    }
 }
