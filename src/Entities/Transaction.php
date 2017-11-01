f790936f73d2754a8cd59ea12ada2ec0cd3c1349
Arman Tuyakbayev <xaossintez@gmail.com>
2017-11-01 15:02:32 +0600

    Add isRetry and retriesResult (#241)

--- src/Entities/Transaction.php
+++ src/Entities/Transaction.php
@@ -307,4 +307,20 @@ final class Transaction extends Entity
     {
         return new Address($data);
     }
+
+    /**
+     * @return bool
+     */
+    public function getIsRetry()
+    {
+        return $this->getAttribute('isRetry');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRetriesResult()
+    {
+        return $this->getAttribute('retriesResult');
+    }
 }
