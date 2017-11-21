10b57e87b22dd3490f74ab0f09eb9c18d7f935b9
Arman Tuyakbayev <xaossintez@gmail.com>
2017-11-21 10:16:50 +0600

    Fix codestyle

--- src/Services/SessionService.php
+++ src/Services/SessionService.php
@@ -15,8 +15,6 @@ use JsonSerializable;
 use Rebilly\Entities\Session;
 use Rebilly\Http\Exception\NotFoundException;
 use Rebilly\Http\Exception\UnprocessableEntityException;
-use Rebilly\Paginator;
-use Rebilly\Rest\Collection;
 use Rebilly\Rest\Service;
 
 /**
