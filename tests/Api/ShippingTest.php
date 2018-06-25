3201caae858da601c994310746e6afabd039e636
Xaos <xaossintez@gmail.com>
2018-06-25 23:33:13 +0600

    Add ShippingTest

---
+++ tests/Api/ShippingTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * This file is part of the PHP Rebilly API package.
+ *
+ * (c) 2015 Rebilly SRL
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Rebilly\Tests\Api;
+
+use DomainException;
+use Rebilly\Entities\PaymentCardMigrationsResponse;
+use Rebilly\Entities\PaymentInstruments\AchInstrument;
+use Rebilly\Entities\PaymentInstruments\CashInstrument;
+use Rebilly\Entities\PaymentInstruments\PaymentCardInstrument;
+use Rebilly\Entities\PaymentInstruments\PaymentCardPaymentInstrument;
+use Rebilly\Entities\PaymentInstruments\PayPalInstrument;
+use Rebilly\Entities\PaymentMethodInstrument;
+use Rebilly\Entities\Shipping\Rate;
+use Rebilly\Tests\TestCase as BaseTestCase;
+
+/**
+ * Class ShippingTest.
+ */
+class ShippingTest extends BaseTestCase
+{
+    /**
+     * @test
+     */
+    public function rate()
+    {
+        $rate = new Rate();
+        $rate->setMinOrderSubtotal(1);
+        $rate->setMaxOrderSubtotal(10);
+        $rate->setCurrency('USD');
+        $rate->setName('test name');
+        $rate->setPrice(1.2);
+
+        self::assertInstanceOf(Rate::class, $rate);
+        self::assertSame(1, $rate->getMinOrderSubtotal());
+        self::assertSame(10, $rate->getMaxOrderSubtotal());
+        self::assertSame(1.2, $rate->getPrice());
+        self::assertSame('USD', $rate->getCurrency());
+        self::assertSame('test name', $rate->getName());
+    }
+
+    /**
+     * @test
+     * @dataProvider provideInvalidData
+     * @expectedException DomainException
+     *
+     * @param $data
+     */
+    public function exceptionOnWrongRequiredField($data)
+    {
+        Rate::createFromData($data);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideInvalidData()
+    {
+        return [
+            'Empty name' => [
+                [
+                    'price' => 1.2,
+                    'currency' => 'USD',
+                ]
+            ],
+            'Empty price' => [
+                [
+                    'name' => 'test name',
+                    'currency' => 'USD',
+                ]
+            ],
+            'Empty currency' => [
+                [
+                    'name' => 'test name',
+                    'price' => 1.2,
+                ]
+            ],
+        ];
+    }
+}
