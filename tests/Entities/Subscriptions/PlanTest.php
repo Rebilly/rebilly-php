0eb7208e7ce431d90efe6988beec8d73061ed92f
Veaceslav Medvedev <slavcopost@gmail.com>
2018-08-25 02:10:55 +0200

    Replace constant array with method ()Darray const is not supported in php5.5

--- tests/Entities/Subscriptions/PlanTest.php
+++ tests/Entities/Subscriptions/PlanTest.php
@@ -12,54 +12,59 @@ use Rebilly\Tests\TestCase;
 
 final class PlanTest extends TestCase
 {
-    const DEFAULT_DATA = [
-        'id' => 'plan-1',
-        'isActive' => true,
-        'name' => 'Plan name',
-        'productId' => 'product-1',
-        'description' => 'Plan description',
-        'richDescription' => 'Plan rich description',
-        'currency' => 'USD',
-        'currencySign' => '$',
-        'pricing' => [
-            'formula' => PlanPricing::FIXED_FEE,
-            'price' => 1.0,
-        ],
-        'recurringInterval' => [
-            'unit' => 'month',
-            'length' => 1,
-            'limit' => 12,
-        ],
-        'trial' => [
-            'price' => 5.0,
-            'period' => [
-                'unit' => 'week',
-                'length' => 2,
+    private static function getDefaultData()
+    {
+        return [
+            'id' => 'plan-1',
+            'isActive' => true,
+            'name' => 'Plan name',
+            'productId' => 'product-1',
+            'description' => 'Plan description',
+            'richDescription' => 'Plan rich description',
+            'currency' => 'USD',
+            'currencySign' => '$',
+            'pricing' => [
+                'formula' => PlanPricing::FIXED_FEE,
+                'price' => 1.0,
             ],
-        ],
-        'setup' => [
-            'price' => 1.0,
-        ],
-    ];
+            'recurringInterval' => [
+                'unit' => 'month',
+                'length' => 1,
+                'limit' => 12,
+            ],
+            'trial' => [
+                'price' => 5.0,
+                'period' => [
+                    'unit' => 'week',
+                    'length' => 2,
+                ],
+            ],
+            'setup' => [
+                'price' => 1.0,
+            ],
+        ];
+    }
 
     /**
      * @test
      */
     public function buildObjectUsingSetterToSendToServer()
     {
-        $value = new Plan();
-        $value->setIsActive(self::DEFAULT_DATA['isActive']);
-        $value->setName(self::DEFAULT_DATA['name']);
-        $value->setProductId(self::DEFAULT_DATA['productId']);
-        $value->setDescription(self::DEFAULT_DATA['description']);
-        $value->setRichDescription(self::DEFAULT_DATA['richDescription']);
-        $value->setCurrency(self::DEFAULT_DATA['currency']);
-        $value->setPricing($value->createPricing(self::DEFAULT_DATA['pricing']));
-        $value->setRecurringInterval($value->createRecurringInterval(self::DEFAULT_DATA['recurringInterval']));
-        $value->setTrial($value->createTrial(self::DEFAULT_DATA['trial']));
-        $value->setSetup($value->createSetup(self::DEFAULT_DATA['setup']));
+        $data = self::getDefaultData();
 
-        $expectedJson = self::DEFAULT_DATA;
+        $value = new Plan();
+        $value->setIsActive($data['isActive']);
+        $value->setName($data['name']);
+        $value->setProductId($data['productId']);
+        $value->setDescription($data['description']);
+        $value->setRichDescription($data['richDescription']);
+        $value->setCurrency($data['currency']);
+        $value->setPricing($value->createPricing($data['pricing']));
+        $value->setRecurringInterval($value->createRecurringInterval($data['recurringInterval']));
+        $value->setTrial($value->createTrial($data['trial']));
+        $value->setSetup($value->createSetup($data['setup']));
+
+        $expectedJson = $data;
         // Unset read-only properties which we not set.
         unset(
             $expectedJson['id'],
@@ -74,23 +79,25 @@ final class PlanTest extends TestCase
      */
     public function populatePlanFromArrayReceivedFromServer()
     {
-        $plan = new Plan(self::DEFAULT_DATA);
+        $data = self::getDefaultData();
 
-        self::assertSame(self::DEFAULT_DATA['id'], $plan->getId());
-        self::assertSame(self::DEFAULT_DATA['isActive'], $plan->getIsActive());
-        self::assertSame(self::DEFAULT_DATA['name'], $plan->getName());
-        self::assertSame(self::DEFAULT_DATA['productId'], $plan->getProductId());
-        self::assertSame(self::DEFAULT_DATA['description'], $plan->getDescription());
-        self::assertSame(self::DEFAULT_DATA['richDescription'], $plan->getRichDescription());
-        self::assertSame(self::DEFAULT_DATA['currency'], $plan->getCurrency());
-        self::assertSame(self::DEFAULT_DATA['currencySign'], $plan->getCurrencySign());
+        $plan = new Plan($data);
+
+        self::assertSame($data['id'], $plan->getId());
+        self::assertSame($data['isActive'], $plan->getIsActive());
+        self::assertSame($data['name'], $plan->getName());
+        self::assertSame($data['productId'], $plan->getProductId());
+        self::assertSame($data['description'], $plan->getDescription());
+        self::assertSame($data['richDescription'], $plan->getRichDescription());
+        self::assertSame($data['currency'], $plan->getCurrency());
+        self::assertSame($data['currencySign'], $plan->getCurrencySign());
         self::assertInstanceOf(FixedFee::class, $plan->getPricing());
-        self::assertSame(self::DEFAULT_DATA['pricing'], $plan->getPricing()->jsonSerialize());
+        self::assertSame($data['pricing'], $plan->getPricing()->jsonSerialize());
         self::assertInstanceOf(RecurringInterval::class, $plan->getRecurringInterval());
-        self::assertSame(self::DEFAULT_DATA['recurringInterval'], $plan->getRecurringInterval()->jsonSerialize());
+        self::assertSame($data['recurringInterval'], $plan->getRecurringInterval()->jsonSerialize());
         self::assertInstanceOf(PlanTrial::class, $plan->getTrial());
-        self::assertSame(self::DEFAULT_DATA['trial'], $plan->getTrial()->jsonSerialize());
+        self::assertSame($data['trial'], $plan->getTrial()->jsonSerialize());
         self::assertInstanceOf(PlanSetup::class, $plan->getSetup());
-        self::assertSame(self::DEFAULT_DATA['setup'], $plan->getSetup()->jsonSerialize());
+        self::assertSame($data['setup'], $plan->getSetup()->jsonSerialize());
     }
 }
