46ec43a9760b1377fb6c3803471a87c9c2e2ec6b
Veaceslav Medvedev <slavcopost@gmail.com>
2018-08-28 13:16:56 +0200

    Remove deprecated attributes and a bit order methods

--- src/Entities/Subscription.php
+++ src/Entities/Subscription.php
@@ -14,22 +14,7 @@ namespace Rebilly\Entities;
 use Rebilly\Rest\Entity;
 
 /**
- * Class Subscription
- *
- * ```json
- * {
- *   "id"
- *   "customerId"
- *   "planId"
- *   "websiteId"
- *   "initialInvoiceId"
- *   "billingContactId"
- *   "deliveryContactId"
- *   "quantity"
- *   "customFields"
- * }
- * ```
- *
+ * Class Subscription.
  */
 final class Subscription extends Entity
 {
@@ -47,18 +32,6 @@ final class Subscription extends Entity
     public function getCustomerId()
     {
         return $this->getAttribute('customerId');
-    }
-
-    /**
-     * @return null|Customer
-     */
-    public function getCustomer()
-    {
-        if ($this->hasEmbeddedResource('customer')) {
-            return new Customer($this->getEmbeddedResource('customer'));
-        }
-
-        return null;
     }
 
     /**
@@ -92,51 +65,9 @@ final class Subscription extends Entity
     /**
      * @return string
      */
-    public function getPlanId()
-    {
-        return $this->getAttribute('planId');
-    }
-
-    /**
-     * @return null|Plan
-     */
-    public function getPlan()
-    {
-        if ($this->hasEmbeddedResource('plan')) {
-            return new Plan($this->getEmbeddedResource('plan'));
-        }
-
-        return null;
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return Subscription
-     */
-    public function setPlanId($value)
-    {
-        return $this->setAttribute('planId', $value);
-    }
-
-    /**
-     * @return string
-     */
     public function getWebsiteId()
     {
         return $this->getAttribute('websiteId');
-    }
-
-    /**
-     * @return null|Website
-     */
-    public function getWebsite()
-    {
-        if ($this->hasEmbeddedResource('website')) {
-            return new Website($this->getEmbeddedResource('website'));
-        }
-
-        return null;
     }
 
     /**
@@ -147,92 +78,6 @@ final class Subscription extends Entity
     public function setWebsiteId($value)
     {
         return $this->setAttribute('websiteId', $value);
-    }
-
-    /**
-     * @return int
-     */
-    public function getQuantity()
-    {
-        return $this->getAttribute('quantity');
-    }
-
-    /**
-     * @param int $value
-     *
-     * @return Subscription
-     */
-    public function setQuantity($value)
-    {
-        return $this->setAttribute('quantity', (int) $value);
-    }
-
-    /**
-     * @deprecated The method is deprecated and will be removed in next version.
-     *
-     * @return string
-     */
-    public function getBillingContactId()
-    {
-        return $this->getAttribute('billingContactId');
-    }
-
-    /**
-     * @deprecated The method is deprecated and will be removed in next version.
-     *
-     * @return null|Contact
-     */
-    public function getBillingContact()
-    {
-        if ($this->hasEmbeddedResource('billingContact')) {
-            return new Contact($this->getEmbeddedResource('billingContact'));
-        }
-
-        return null;
-    }
-
-    /**
-     * @deprecated The method is deprecated and will be removed in next version.
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setBillingContactId($value)
-    {
-        return $this->setAttribute('billingContactId', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getDeliveryContactId()
-    {
-        return $this->getAttribute('deliveryContactId');
-    }
-
-    /**
-     * @deprecated The method is deprecated and will be removed in next version.
-     *
-     * @return null|Contact
-     */
-    public function getDeliveryContact()
-    {
-        if ($this->hasEmbeddedResource('deliveryContact')) {
-            return new Contact($this->getEmbeddedResource('deliveryContact'));
-        }
-
-        return null;
-    }
-
-    /**
-     * @deprecated The method is deprecated and will be removed in next version.
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setDeliveryContactId($value)
-    {
-        return $this->setAttribute('deliveryContactId', $value);
     }
 
     /**
@@ -268,14 +113,6 @@ final class Subscription extends Entity
     }
 
     /**
-     * @return string
-     */
-    public function getCreatedTime()
-    {
-        return $this->getAttribute('createdTime');
-    }
-
-    /**
      * @param string $value
      *
      * @return $this
@@ -283,6 +120,14 @@ final class Subscription extends Entity
     public function setRenewalTime($value)
     {
         return $this->setAttribute('renewalTime', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedTime()
+    {
+        return $this->getAttribute('createdTime');
     }
 
     /**
@@ -392,6 +237,16 @@ final class Subscription extends Entity
     }
 
     /**
+     * @param array $data
+     *
+     * @return Address
+     */
+    public function createBillingAddress(array $data)
+    {
+        return new Address($data);
+    }
+
+    /**
      * @return Address
      */
     public function getDeliveryAddress()
@@ -407,6 +262,16 @@ final class Subscription extends Entity
     public function setDeliveryAddress($value)
     {
         return $this->setAttribute('deliveryAddress', $value);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return Address
+     */
+    public function createDeliveryAddress(array $data)
+    {
+        return new Address($data);
     }
 
     /**
@@ -428,26 +293,6 @@ final class Subscription extends Entity
     }
 
     /**
-     * @param array $data
-     *
-     * @return Address
-     */
-    public function createBillingAddress(array $data)
-    {
-        return new Address($data);
-    }
-
-    /**
-     * @param array $data
-     *
-     * @return Address
-     */
-    public function createDeliveryAddress(array $data)
-    {
-        return new Address($data);
-    }
-
-    /**
      * @return array
      */
     public function getLineItems()
@@ -461,5 +306,29 @@ final class Subscription extends Entity
     public function getLineItemSubtotal()
     {
         return $this->getAttribute('lineItemSubtotal');
+    }
+
+    /**
+     * @return null|Website
+     */
+    public function getWebsite()
+    {
+        if ($this->hasEmbeddedResource('website')) {
+            return new Website($this->getEmbeddedResource('website'));
+        }
+
+        return null;
+    }
+
+    /**
+     * @return null|Customer
+     */
+    public function getCustomer()
+    {
+        if ($this->hasEmbeddedResource('customer')) {
+            return new Customer($this->getEmbeddedResource('customer'));
+        }
+
+        return null;
     }
 }
