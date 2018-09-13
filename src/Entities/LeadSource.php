0a4b58ddc2c037fa90116635acb8a5ca754fdede
Arman Tuyakbayev <xaossintez@gmail.com>
2018-09-13 16:42:02 +0600

    Adjust LeadSource to latest changes (#298)
    * Adjust LeadSource to latest changes
    
    * Redo test
    
    * Remove wrong imports
    
    * Fix test
    
    * Add changelog
    
    * Remove ipAddress

--- src/Entities/LeadSource.php
+++ src/Entities/LeadSource.php
@@ -11,13 +11,12 @@
 
 namespace Rebilly\Entities;
 
-use Rebilly\Rest\Entity;
-
 /**
  * Class LeadSource
  *
  * ```json
  * {
+ *   "original"
  *   "customerId"
  *   "medium"
  *   "source"
@@ -29,264 +28,18 @@ use Rebilly\Rest\Entity;
  *   "salesAgent"
  *   "clickId"
  *   "path"
- *   "ipAddress"
  *   "currency"
  *   "amount"
  * }
  * ```
- *
  */
-final class LeadSource extends Entity
+final class LeadSource extends LeadSourceData
 {
     /**
-     * @return string
+     * @return LeadSourceData
      */
-    public function getCustomerId()
+    public function getOriginal()
     {
-        return $this->getAttribute('customerId');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setCustomerId($value)
-    {
-        return $this->setAttribute('customerId', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getMedium()
-    {
-        return $this->getAttribute('medium');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setMedium($value)
-    {
-        return $this->setAttribute('medium', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getSource()
-    {
-        return $this->getAttribute('source');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setSource($value)
-    {
-        return $this->setAttribute('source', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getCampaign()
-    {
-        return $this->getAttribute('campaign');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setCampaign($value)
-    {
-        return $this->setAttribute('campaign', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getTerm()
-    {
-        return $this->getAttribute('term');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setTerm($value)
-    {
-        return $this->setAttribute('term', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->getAttribute('content');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setContent($value)
-    {
-        return $this->setAttribute('content', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getAffiliate()
-    {
-        return $this->getAttribute('affiliate');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setAffiliate($value)
-    {
-        return $this->setAttribute('affiliate', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getSubAffiliate()
-    {
-        return $this->getAttribute('subAffiliate');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setSubAffiliate($value)
-    {
-        return $this->setAttribute('subAffiliate', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getSalesAgent()
-    {
-        return $this->getAttribute('salesAgent');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setSalesAgent($value)
-    {
-        return $this->setAttribute('salesAgent', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getClickId()
-    {
-        return $this->getAttribute('clickId');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setClickId($value)
-    {
-        return $this->setAttribute('clickId', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getPath()
-    {
-        return $this->getAttribute('path');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setPath($value)
-    {
-        return $this->setAttribute('path', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getIpAddress()
-    {
-        return $this->getAttribute('ipAddress');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setIpAddress($value)
-    {
-        return $this->setAttribute('ipAddress', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getAmount()
-    {
-        return $this->getAttribute('amount');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setAmount($value)
-    {
-        return $this->setAttribute('amount', $value);
-    }
-
-    /**
-     * @return string
-     */
-    public function getCurrency()
-    {
-        return $this->getAttribute('currency');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return $this
-     */
-    public function setCurrency($value)
-    {
-        return $this->setAttribute('currency', $value);
+        return new LeadSourceData((array) $this->getAttribute('original'));
     }
 }
