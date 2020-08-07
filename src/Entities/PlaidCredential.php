b4aab791cc727d0045f3cf88edb45c83ff8161b1
Veaceslav Medvedev <slavcopost@gmail.com>
2020-08-07 13:04:45 +0200

    Add PlaidCredential entity

---
+++ src/Entities/PlaidCredential.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * This source file is proprietary and part of Rebilly.
+ *
+ * (c) Rebilly SRL
+ *     Rebilly Ltd.
+ *     Rebilly Inc.
+ *
+ * @see https://www.rebilly.com
+ */
+
+namespace Rebilly\Entities;
+
+use Rebilly\Rest\Entity;
+
+final class PlaidCredential extends Entity
+{
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->getHash();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash()
+    {
+        return $this->getAttribute('hash');
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->getAttribute('status');
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setStatus($value)
+    {
+        return $this->setAttribute('status', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebsiteId()
+    {
+        return $this->getAttribute('websiteId');
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setWebsiteId($value)
+    {
+        return $this->setAttribute('websiteId', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientId()
+    {
+        return $this->getAttribute('clientId');
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setClientId($value)
+    {
+        return $this->setAttribute('clientId', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecretToken()
+    {
+        return $this->getAttribute('secretToken');
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setSecretToken($value)
+    {
+        return $this->setAttribute('secretToken', $value);
+    }
+
+    /**
+     * @return bool
+     */
+    public function getUseStripe()
+    {
+        return $this->getAttribute('useStripe');
+    }
+
+    /**
+     * @param bool $value
+     *
+     * @return $this
+     */
+    public function setUseStripe($value)
+    {
+        return $this->setAttribute('useStripe', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeactivationTime()
+    {
+        return $this->getAttribute('deactivationTime');
+    }
+}
