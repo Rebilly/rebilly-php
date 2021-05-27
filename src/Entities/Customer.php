db4833bfe80c232221d4f318a8c4346252365806
Roman Chernyshov <romanchernyshof@gmail.com>
2021-05-27 15:32:24 +0300

    Add getTags method to Customer (#476)
    * Add getTags method to Customer
    
    * Update CHANGELOG.md
    
    Co-authored-by: Alexander Shkarpetin <ashkarpetin@gmail.com>

--- src/Entities/Customer.php
+++ src/Entities/Customer.php
@@ -375,4 +375,24 @@ final class Customer extends Entity
     {
         return $this->getAttribute('revision');
     }
+
+    /**
+     * @return array|Tag[]
+     */
+    public function getTags()
+    {
+        return $this->getAttribute('tags');
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array|Tag[]
+     */
+    public function createTags(array $data)
+    {
+        return array_map(function ($element) {
+            return new Tag($element);
+        }, $data);
+    }
 }
