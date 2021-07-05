bf7422256f67dcc18ef91a110e950c9cf4993e1c
svendenotter <sven.den.otter@rebilly.com>
2021-07-05 15:54:21 +0900

    Add update and delete methods to invoice item service

--- src/Services/InvoiceItemService.php
+++ src/Services/InvoiceItemService.php
@@ -99,4 +99,37 @@ final class InvoiceItemService extends Service
             ['invoiceId' => $invoiceId]
         );
     }
+
+    /**
+     * @param array|JsonSerializable|InvoiceItem $data
+     * @param string $invoiceId
+     * @param string $invoiceItemId
+     *
+     * @throws DataValidationException The input data does not valid
+     *
+     * @return InvoiceItem
+     */
+    public function update($data, $invoiceId, $invoiceItemId)
+    {
+        return $this->client()->put(
+            $data,
+            'invoices/{invoiceId}/items/{invoiceItemId}',
+            ['invoiceId' => $invoiceId, 'invoiceItemId' => $invoiceItemId]
+        );
+    }
+
+    /**
+     * @param array|JsonSerializable|InvoiceItem $data
+     * @param string $invoiceId
+     * @param string $invoiceItemId
+     *
+     * @return InvoiceItem
+     */
+    public function delete($data, $invoiceId, $invoiceItemId)
+    {
+        return $this->client()->delete(
+            'invoices/{invoiceId}/items/{invoiceItemId}',
+            ['invoiceId' => $invoiceId, 'invoiceItemId' => $invoiceItemId]
+        );
+    }
 }
