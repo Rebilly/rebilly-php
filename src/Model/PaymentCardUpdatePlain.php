7dbc8fd54782108cff20c775462556e8e4173a1a
rebilly-machine-user <rebilly-machine-user@users.noreply.github.com>
2023-09-11 20:33:36 +0000

    update SDK from api-definitions

---
+++ src/Model/PaymentCardUpdatePlain.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * This source file is proprietary and part of Rebilly.
+ *
+ * (c) Rebilly SRL
+ *     Rebilly Ltd.
+ *     Rebilly Inc.
+ *
+ * @see https://www.rebilly.com
+ */
+
+declare(strict_types=1);
+
+namespace Rebilly\Sdk\Model;
+
+use JsonSerializable;
+
+class PaymentCardUpdatePlain implements PatchPaymentInstrumentRequest, JsonSerializable
+{
+    public const ACCOUNT_TYPE_CHECKING = 'checking';
+
+    public const ACCOUNT_TYPE_SAVINGS = 'savings';
+
+    public const ACCOUNT_TYPE_OTHER = 'other';
+
+    private array $fields = [];
+
+    public function __construct(array $data = [])
+    {
+        if (array_key_exists('cvv', $data)) {
+            $this->setCvv($data['cvv']);
+        }
+        if (array_key_exists('expMonth', $data)) {
+            $this->setExpMonth($data['expMonth']);
+        }
+        if (array_key_exists('expYear', $data)) {
+            $this->setExpYear($data['expYear']);
+        }
+        if (array_key_exists('billingAddress', $data)) {
+            $this->setBillingAddress($data['billingAddress']);
+        }
+        if (array_key_exists('stickyGatewayAccountId', $data)) {
+            $this->setStickyGatewayAccountId($data['stickyGatewayAccountId']);
+        }
+        if (array_key_exists('customFields', $data)) {
+            $this->setCustomFields($data['customFields']);
+        }
+        if (array_key_exists('useAsBackup', $data)) {
+            $this->setUseAsBackup($data['useAsBackup']);
+        }
+        if (array_key_exists('accountType', $data)) {
+            $this->setAccountType($data['accountType']);
+        }
+        if (array_key_exists('bankName', $data)) {
+            $this->setBankName($data['bankName']);
+        }
+        if (array_key_exists('token', $data)) {
+            $this->setToken($data['token']);
+        }
+    }
+
+    public static function from(array $data = []): self
+    {
+        return new self($data);
+    }
+
+    public function getCvv(): ?string
+    {
+        return $this->fields['cvv'] ?? null;
+    }
+
+    public function setCvv(null|string $cvv): static
+    {
+        $this->fields['cvv'] = $cvv;
+
+        return $this;
+    }
+
+    public function getExpMonth(): ?int
+    {
+        return $this->fields['expMonth'] ?? null;
+    }
+
+    public function setExpMonth(null|int $expMonth): static
+    {
+        $this->fields['expMonth'] = $expMonth;
+
+        return $this;
+    }
+
+    public function getExpYear(): ?int
+    {
+        return $this->fields['expYear'] ?? null;
+    }
+
+    public function setExpYear(null|int $expYear): static
+    {
+        $this->fields['expYear'] = $expYear;
+
+        return $this;
+    }
+
+    public function getBillingAddress(): ?ContactObject
+    {
+        return $this->fields['billingAddress'] ?? null;
+    }
+
+    public function setBillingAddress(null|ContactObject|array $billingAddress): static
+    {
+        if ($billingAddress !== null && !($billingAddress instanceof ContactObject)) {
+            $billingAddress = ContactObject::from($billingAddress);
+        }
+
+        $this->fields['billingAddress'] = $billingAddress;
+
+        return $this;
+    }
+
+    public function getStickyGatewayAccountId(): ?string
+    {
+        return $this->fields['stickyGatewayAccountId'] ?? null;
+    }
+
+    public function setStickyGatewayAccountId(null|string $stickyGatewayAccountId): static
+    {
+        $this->fields['stickyGatewayAccountId'] = $stickyGatewayAccountId;
+
+        return $this;
+    }
+
+    public function getCustomFields(): ?array
+    {
+        return $this->fields['customFields'] ?? null;
+    }
+
+    public function setCustomFields(null|array $customFields): static
+    {
+        $this->fields['customFields'] = $customFields;
+
+        return $this;
+    }
+
+    public function getUseAsBackup(): ?bool
+    {
+        return $this->fields['useAsBackup'] ?? null;
+    }
+
+    public function setUseAsBackup(null|bool $useAsBackup): static
+    {
+        $this->fields['useAsBackup'] = $useAsBackup;
+
+        return $this;
+    }
+
+    public function getAccountType(): ?string
+    {
+        return $this->fields['accountType'] ?? null;
+    }
+
+    public function setAccountType(null|string $accountType): static
+    {
+        $this->fields['accountType'] = $accountType;
+
+        return $this;
+    }
+
+    public function getBankName(): ?string
+    {
+        return $this->fields['bankName'] ?? null;
+    }
+
+    public function setBankName(null|string $bankName): static
+    {
+        $this->fields['bankName'] = $bankName;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->fields['token'] ?? null;
+    }
+
+    public function setToken(null|string $token): static
+    {
+        $this->fields['token'] = $token;
+
+        return $this;
+    }
+
+    public function jsonSerialize(): array
+    {
+        $data = [];
+        if (array_key_exists('cvv', $this->fields)) {
+            $data['cvv'] = $this->fields['cvv'];
+        }
+        if (array_key_exists('expMonth', $this->fields)) {
+            $data['expMonth'] = $this->fields['expMonth'];
+        }
+        if (array_key_exists('expYear', $this->fields)) {
+            $data['expYear'] = $this->fields['expYear'];
+        }
+        if (array_key_exists('billingAddress', $this->fields)) {
+            $data['billingAddress'] = $this->fields['billingAddress']?->jsonSerialize();
+        }
+        if (array_key_exists('stickyGatewayAccountId', $this->fields)) {
+            $data['stickyGatewayAccountId'] = $this->fields['stickyGatewayAccountId'];
+        }
+        if (array_key_exists('customFields', $this->fields)) {
+            $data['customFields'] = $this->fields['customFields'];
+        }
+        if (array_key_exists('useAsBackup', $this->fields)) {
+            $data['useAsBackup'] = $this->fields['useAsBackup'];
+        }
+        if (array_key_exists('accountType', $this->fields)) {
+            $data['accountType'] = $this->fields['accountType'];
+        }
+        if (array_key_exists('bankName', $this->fields)) {
+            $data['bankName'] = $this->fields['bankName'];
+        }
+        if (array_key_exists('token', $this->fields)) {
+            $data['token'] = $this->fields['token'];
+        }
+
+        return $data;
+    }
+}
